c6564b95fa8609ea74549c3dfb1883e6eab70eec
Otto Eklund <ottoeklund@gmail.com>
2020-01-29 11:05:16 +0200

    eka commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require("node-fetch");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  if (req.body === {}){
+    
+  }
+    
 });
+router.post("/", function(req,res,next){
+  res.send(req.body);
+  console.log(req.body);
+})
 
 module.exports = router;
